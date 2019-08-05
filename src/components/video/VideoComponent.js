6219bdd19224b5d302255c3b7408d049f0ac40c7
tr670294 <wagner.magno.r.sales@accenture.com>
2019-08-05 12:08:00 -0300

    novo teste no video

--- src/components/video/VideoComponent.js
+++ src/components/video/VideoComponent.js
@@ -13,12 +13,13 @@ export default class VideoComponent extends React.Component {
 
         const videoOptions = {
             // src: 'https://gcs-vimeo.akamaized.net/exp=1563303566~acl=%2A%2F1294263193.mp4%2A~hmac=8b51ca4a8fb340c658ecaa980cb37a05be63485634d8d83d17eac1d864213584/vimeo-prod-skyfire-std-us/01/955/13/329779326/1294263193.mp4',
-                  src: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+            src: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
 
+            autoPlay: true,
             ref: videoRef => {
                 this.videoRef = videoRef;
+                this.videoRef.play()
             },
-            autoPlay: true,
             loop: true,
             onClick: () => {
                 if (this.videoRef && this.videoRef.paused) {
@@ -36,7 +37,7 @@ export default class VideoComponent extends React.Component {
                 top: "0px",
                 left: "0px",
                 zIndex: 1,
-                cursor : "pointer" //`url(${cursor}), default)`
+                cursor: "pointer" //`url(${cursor}), default)`
             }}>
                 <VideoCover
                     videoOptions={videoOptions}
