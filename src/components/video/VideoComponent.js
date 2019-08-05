f3e76df6bf271b4d1d8d739957e26fbeb8f7adb7
tr670294 <wagner.magno.r.sales@accenture.com>
2019-08-05 12:14:34 -0300

    mudança do video

--- src/components/video/VideoComponent.js
+++ src/components/video/VideoComponent.js
@@ -13,13 +13,12 @@ export default class VideoComponent extends React.Component {
 
         const videoOptions = {
             // src: 'https://gcs-vimeo.akamaized.net/exp=1563303566~acl=%2A%2F1294263193.mp4%2A~hmac=8b51ca4a8fb340c658ecaa980cb37a05be63485634d8d83d17eac1d864213584/vimeo-prod-skyfire-std-us/01/955/13/329779326/1294263193.mp4',
-            src: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+            src: 'https://player.vimeo.com/external/329779326.hd.mp4?s=d65c7e3f09e39facf31efc6a283df33c2391960e&profile_id=175',
 
-            autoPlay: true,
             ref: videoRef => {
                 this.videoRef = videoRef;
-                this.videoRef.play()
             },
+            autoPlay: true,
             loop: true,
             onClick: () => {
                 if (this.videoRef && this.videoRef.paused) {
