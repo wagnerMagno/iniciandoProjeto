f860bc5e551d41ec1d5e8c57eba1faf12f8c0dcf
tr670294 <wagner.magno.r.sales@accenture.com>
2019-08-28 17:09:35 -0300

    ajuste

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -22,18 +22,26 @@ export default class HomeComponent extends React.Component {
     render() {
         return (
             <HomeComponentStyled className={this.props.className}>
-                <p className="label">
-                    F&F Administradora.
-                </p>
-                <p className="descricao">
-                    Desde de 2010 a F&F Administradora sempre apresenta soluções <br></br>
-                    inovadoras sempre trazendo redução de custos e mais transparência <br></br>
-                    para gestão do seu condomínio.
-                </p>
+                {
+                    window.innerWidth > 700 ?
+                        <div>
+
+                            <p className="label">
+                                F&F Administradora.
+                            </p>
+                            <p className="descricao">
+                                Desde de 2010 a F&F Administradora sempre apresenta soluções <br></br>
+                                inovadoras sempre trazendo redução de custos e mais transparência <br></br>
+                                para gestão do seu condomínio.
+                            </p>
+                        </div>
+                        :
+                        ''
+                }
 
                 <div className="title">
                     <div class="info">
-                    <Link style={{color: 'white', textDecoration: "none"}} to="/digio-final">
+                        <Link style={{ color: 'white', textDecoration: "none" }} to="/digio-final">
                             <p className="p-title">
                                 TUDO QUE SEU CONDOMÍNIO PRECISA E AINDA PAGANDO MENOS
                             </p>
